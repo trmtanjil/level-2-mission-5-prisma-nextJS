8a16bdda8a5c7dd37902d636b0cf8d41c9666fee
trmtanjil <trmtanjil02@gmail.com>
2025-12-29 12:15:57 +0600

    Updating and deleting records

--- moddull-22-prisma/crud.ts
+++ moddull-22-prisma/crud.ts
@@ -32,21 +32,77 @@ async function run() {
     // console.log(createProfil)
 
     // retrive all user
-    const users= await prisma.user.findMany({
-        // include:{
-        //     posts:true,
-        //     profils:true
-        // }
+    // const users= await prisma.user.findMany({
+    //     // include:{
+    //     //     posts:true,
+    //     //     profils:true
+    //     // }
 
-               select:{
-                id:true,
-                name:true,
-                email:true,
-            posts:true,
-            profils:true
-        }
-    });
-    console.dir(users,{depth:Infinity})
+    //            select:{
+    //             id:true,
+    //             name:true,
+    //             email:true,
+    //         posts:true,
+    //         profils:true
+    //     }
+    // });
+    // console.dir(users,{depth:Infinity})
+
+
+
+// const updateUser = await prisma.profil.update({
+//     where:{
+//         userId:1
+//     },
+//     data:{
+//         bio:'im student at ph level 2',
+//         dateofBirth:'2025-12-29T04:59:47.373Z'
+//     },
+//     select:{
+//         id:true,
+//         bio:true,
+//         user:{
+//             select:{
+//                 name:true,
+//                 email:true
+//             }
+//         },
+//     }
+// })
+
+// console.log('update user',updateUser)
+
+
+
+// const deletedUser = await prisma.user.delete({
+//     where:{
+//         id:2
+//     }
+// })
+// console.log(deletedUser)
+
+
+
+//get user data by id
+const getuserDatabyId = await prisma.user.findUnique({
+    where:{
+        id:2,
+    },
+    include:{
+        posts:true,
+        profils:true
+    }
+})
+console.log(getuserDatabyId)
+
+
+
+
+
+
+
+
+
 }
 
 run()
