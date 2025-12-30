3afb61661460bba426a0e64ab11a50682d33de69
trmtanjil <trmtanjil02@gmail.com>
2025-12-30 11:54:51 +0600

    Express Server & Application Configuration

---
+++ moddule-23-prisma-blog-app/src/server.ts
@@ -0,0 +1,20 @@
+import app from "./app";
+import { prisma } from "./lib/prisma";
+
+const PORT = process.env.PORT || 5000;
+
+async function main() {
+    try{
+        await prisma.$connect();
+        console.log('connected to the database successfully.');
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+    }catch(error){
+        console.log('an error occurred:', error)
+        await prisma.$disconnect();
+        process.exit(1);
+    }
+}
+main();
