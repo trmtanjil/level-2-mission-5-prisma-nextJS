2c87d81ae1fedf260d488a8e5201042108efa1b7
trmtanjil <trmtanjil02@gmail.com>
2025-12-30 15:26:35 +0600

    Inroduction to Email & Password Authentication (Better Auth Setup)

--- moddule-23-prisma-blog-app/src/app.ts
+++ moddule-23-prisma-blog-app/src/app.ts
@@ -1,10 +1,14 @@
 import express, { Application } from "express";
 import { postRouter } from "./modules/post/post.router";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+
 
 const app: Application = express();
 app.use(express.json());
 
 app.use("/post",postRouter)
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 
 
