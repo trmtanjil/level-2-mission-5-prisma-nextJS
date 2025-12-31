ede4668bb9100104295aee1fdf4641a8e0ba849e
trmtanjil <trmtanjil02@gmail.com>
2026-01-01 00:21:04 +0600

    Email Verification System – Part 1 (Token & Flow Design)

--- moddule-23-prisma-blog-app/src/lib/auth.ts
+++ moddule-23-prisma-blog-app/src/lib/auth.ts
@@ -1,6 +1,21 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
+import nodemailer from "nodemailer"
+
+
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // Use true for port 465, false for port 587
+  auth: {
+    user: process.env.APP_USER,
+    pass: process.env.APP_PASS
+  },
+});
+
+
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
@@ -32,7 +47,17 @@ export const auth = betterAuth({
   },
   emailVerification:{
     sendVerificationEmail:async({user,url,token},request)=>{
-      console.log('********  verification email send !')
+      const verificationUrl = `${process.env.APP_URL}/verify-email?token=${token}`
+
+      const info = await transporter.sendMail({
+    from: '"prisma" <prisma@trm.email>',
+    to: "trmtanjil02@gmail.com",
+    subject: "Hello ✔",
+    text: "Hello world?", // Plain-text version of the message
+    html: "<b>Hello world?</b>", // HTML version of the message
+  });
+
+  console.log("Message sent:", info.messageId);
     }
   }
 });
