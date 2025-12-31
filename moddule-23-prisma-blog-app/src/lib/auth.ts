3e552ebc2e5fbc47ffbf0eb95ff36df3452db86d
trmtanjil <trmtanjil02@gmail.com>
2025-12-31 21:42:58 +0600

    Extending the User Model: Adding Additional User Fields

--- moddule-23-prisma-blog-app/src/lib/auth.ts
+++ moddule-23-prisma-blog-app/src/lib/auth.ts
@@ -6,6 +6,25 @@ export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
+    trustedOrigins:[process.env.APP_URL!],
+    user:{
+      additionalFields:{
+        role:{
+          type :"string",
+          defaultValue:"USER",
+          required:false
+        },
+        phone:{
+          type:"string",
+          required:false
+        },
+        status:{
+          type:"string",
+          defaultValue:"ACTIVE",
+          required:false
+        }
+      }
+    },
       emailAndPassword: { 
     enabled: true, 
   },
