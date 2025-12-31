de2ddf91849b371c7313c286d95bf0b908a20f1d
trmtanjil <trmtanjil02@gmail.com>
2025-12-31 22:51:55 +0600

    Fixing Authentication Errors & Implementing Email Verification

--- moddule-23-prisma-blog-app/src/app.ts
+++ moddule-23-prisma-blog-app/src/app.ts
@@ -2,9 +2,16 @@ import express, { Application } from "express";
 import { postRouter } from "./modules/post/post.router";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
-
+import cors from 'cors';
 
 const app: Application = express();
+
+
+app.use(cors({
+origin:process.env.APP_URL|| "http://localhost:4000",
+credentials:true
+}))
+
 app.use(express.json());
 
 app.use("/post",postRouter)
--- moddule-23-prisma-blog-app/src/lib/auth.ts
+++ moddule-23-prisma-blog-app/src/lib/auth.ts
@@ -27,5 +27,12 @@ export const auth = betterAuth({
     },
       emailAndPassword: { 
     enabled: true, 
+    autoSignIn:false,
+    requireEmailVerification:true
   },
+  emailVerification:{
+    sendVerificationEmail:async({user,url,token},request)=>{
+      console.log('********  verification email send !')
+    }
+  }
 });
