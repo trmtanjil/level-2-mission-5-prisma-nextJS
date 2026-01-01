d9d1064ed5944c761f5c954945bda87878283ce8
trmtanjil <trmtanjil02@gmail.com>
2026-01-01 11:12:16 +0600

    Authentication Middleware – Part 1 (Basic Protection)

--- moddule-23-prisma-blog-app/src/modules/post/post.router.ts
+++ moddule-23-prisma-blog-app/src/modules/post/post.router.ts
@@ -1,10 +1,25 @@
-import  express, { Router }  from "express";
+import  express, { NextFunction, Request, Response, Router }  from "express";
 import { PostController } from "./post.controller";
+import {auth as betterAuth} from "../../lib/auth"
 
 const router = express.Router();
 
+const auth =(...roles:any)=>{
+return async (req:Request,res:Response,next:NextFunction)=>{
+    //get session
+    const session = await betterAuth.api.getSession({
+        headers:req.headers as any 
+    })
+    console.log(session)
+    next()
+}
+}
+
+
 router.post(
-    '/',PostController.createPost
+    '/',
+    auth("USER"),
+    PostController.createPost
 )
 
 
