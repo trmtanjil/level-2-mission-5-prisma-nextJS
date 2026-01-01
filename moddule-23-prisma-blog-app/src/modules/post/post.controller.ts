2c8d8de93dd864fd576947b1b3690ec00f2c8a5d
trmtanjil <trmtanjil02@gmail.com>
2026-01-01 14:28:54 +0600

    Authentication Middleware  Part 3 (Production-Ready Security)

--- moddule-23-prisma-blog-app/src/modules/post/post.controller.ts
+++ moddule-23-prisma-blog-app/src/modules/post/post.controller.ts
@@ -4,8 +4,15 @@ import { postService } from "./post.service";
 
  const createPost = async (req:Request, res:Response)=>{
       try{
+        const user=req.user
+        if(!user){
+         return  res.status(400).json({
+            error:"unauthorized",
+            
+        })
+        }
         console.log(req.user)
-        const result = await postService.createPost(req.body)
+        const result = await postService.createPost(req.body,user.id as string)
         res.status(201).json(result)
       }catch(e){
         res.status(400).json({
--- moddule-23-prisma-blog-app/src/modules/post/post.router.ts
+++ moddule-23-prisma-blog-app/src/modules/post/post.router.ts
@@ -1,70 +1,11 @@
-import  express, { NextFunction, Request, Response, Router }  from "express";
+import  express, {Router}  from "express";
 import { PostController } from "./post.controller";
-import {auth as betterAuth} from "../../lib/auth"
+import auth, { UserRole } from "../../middalewared/auth";
 
 const router = express.Router();
 
 // type for role 
-export enum UserRole{
-    USER="USER",
-    ADMIN="ADMIN"
-}
 
-
-declare global{
-    namespace Express{
-        interface Request{
-            user?:{
-                id:string,
-                email:string,
-                name:string,
-                role:string,
-                emailVerified:boolean;
-            }
-        }
-    }
-}
-
-const auth =(...roles:UserRole[])=>{
-return async (req:Request,res:Response,next:NextFunction)=>{
-    //get session
-    const session = await betterAuth.api.getSession({
-        headers:req.headers as any 
-    })
-
-    if(!session){
-        return res.status(401).json({
-            success:false,
-            message:"you are not authorised!"
-        })
-    }
-
-    if(!session.user.emailVerified){
-        return res.status(403).json({
-            success:false,
-            message:"email varification required Please verify your email"
-        })
-    }
-
-    req.user={
-        id:session.user.id,
-        email:session.user.email,
-        name:session.user.name,
-        role:session.user.role as string,
-        emailVerified:session.user.emailVerified
-    }
-
-
-    if(roles.length && !roles.includes(req.user.role as UserRole)){
-        return res.status(403).json({
-            success:false,
-            message:"Forbidden! you don't have permission to access this resurces!"
-        })
-    }
-
-    next()
-}
-}
 
 
 router.post(
