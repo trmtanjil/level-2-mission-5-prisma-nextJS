311ae7185e422686840eb9f90685330fc13f8670
trmtanjil <trmtanjil02@gmail.com>
2026-01-05 11:51:07 +0600

    Create Comment Part 2

--- moddule-23-prisma-blog-app/src/modules/Comments/comment.service.ts
+++ moddule-23-prisma-blog-app/src/modules/Comments/comment.service.ts
@@ -1,10 +1,29 @@
+import { prisma } from "../../lib/prisma"
+
 const createComment = async(payload:{
     content:string,
     authorId:string,
     postId:string,
     parantId?:string
 })=>{
-    console.log('comment create ',payload)
+
+ await prisma.post.findUniqueOrThrow({
+        where:{
+            id:payload.postId
+        }
+    })
+
+    if(payload.parantId){
+         await prisma.coment.findFirstOrThrow({
+            where:{
+                id:payload.parantId
+            }
+        })
+    }
+
+   return  await prisma.coment.create ({
+    data:payload
+   })
 }
 
 
