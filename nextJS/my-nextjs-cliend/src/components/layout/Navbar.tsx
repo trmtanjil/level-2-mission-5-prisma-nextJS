e0cbf3160fd648dd2bc25c64b51e7de48c11d057
trmtanjil <trmtanjil02@gmail.com>
2026-01-13 21:53:31 +0600

    shadcd router reload solve and a tag validation

--- nextJS/my-nextjs-cliend/src/components/layout/Navbar.tsx
+++ nextJS/my-nextjs-cliend/src/components/layout/Navbar.tsx
@@ -26,6 +26,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import Link from "next/link";
 
 interface MenuItem {
   title: string;
@@ -183,28 +184,15 @@ const Navbar = ({
 };
 
 const renderMenuItem = (item: MenuItem) => {
-  if (item.items) {
-    return (
-      <NavigationMenuItem key={item.title}>
-        <NavigationMenuTrigger>{item.title}</NavigationMenuTrigger>
-        <NavigationMenuContent className="bg-popover text-popover-foreground">
-          {item.items.map((subItem) => (
-            <NavigationMenuLink asChild key={subItem.title} className="w-80">
-              <SubMenuLink item={subItem} />
-            </NavigationMenuLink>
-          ))}
-        </NavigationMenuContent>
-      </NavigationMenuItem>
-    );
-  }
+ 
 
   return (
     <NavigationMenuItem key={item.title}>
       <NavigationMenuLink
-        href={item.url}
+       asChild
         className="group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-muted hover:text-accent-foreground"
       >
-        {item.title}
+      <Link href={item.url}> {item.title}</Link> 
       </NavigationMenuLink>
     </NavigationMenuItem>
   );
