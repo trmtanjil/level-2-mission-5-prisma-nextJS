566526e333181b5cd167ad8dabe97af0f05027b2
trmtanjil <trmtanjil02@gmail.com>
2026-01-15 00:56:01 +0600

    Understanding loading.js and error.js files

--- nextJS/my-nextjs-cliend/src/app/about/page.tsx
+++ nextJS/my-nextjs-cliend/src/app/about/page.tsx
@@ -4,7 +4,7 @@ export  default async function Aboutpage() {
 
  await new Promise((resolve)=>setTimeout(resolve, 4000))
 
- throw new Error ("something went wrong")
+ throw new Error ("something    went wrong")
 
   return (
     <div>this is Aboutpage</div>
