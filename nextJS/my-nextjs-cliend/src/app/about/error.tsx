fd5ecb45545b84f62c3e11631afed516daeda34c
trmtanjil <trmtanjil02@gmail.com>
2026-01-14 17:13:14 +0600

    Understanding loading.js and error.js files

--- nextJS/my-nextjs-cliend/src/app/about/error.tsx
+++ nextJS/my-nextjs-cliend/src/app/about/error.tsx
@@ -1,1 +1,21 @@
+"use client"
+import React, { useEffect } from 'react'
 
+export default function AboutErrot({error, reset}:
+    {error:Error &{digest?:string};
+    reset:()=>void
+}) {
+
+    useEffect(()=>{
+        console.log(error)
+    },[])
+
+
+
+  return (
+<div>
+        <h1>something went wrong plese try again  </h1>
+    <button onClick={()=>reset()}>retry</button>
+</div>
+  )
+}
