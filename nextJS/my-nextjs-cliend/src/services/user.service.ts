9ffecadf539a093a690d32bb5150203b109c3163
trmtanjil <trmtanjil02@gmail.com>
2026-01-20 23:48:01 +0600

    Environment Variables in Nextjs

--- nextJS/my-nextjs-cliend/src/services/user.service.ts
+++ nextJS/my-nextjs-cliend/src/services/user.service.ts
@@ -1,12 +1,15 @@
 import { cookies } from "next/headers"
 
+
+const AUTH_URL= process.env.AUTH_URL
+
 export const userService ={
 getSession: async function () {
    try {
        const cookieStore =await cookies()
   console.log("cokie store",cookieStore.toString())
  
-  const res = await fetch("http://localhost:5000/api/auth/get-session",{
+  const res = await fetch(`${AUTH_URL}/get-session`,{
     headers:{
       Cookie:cookieStore.toString()
     },
