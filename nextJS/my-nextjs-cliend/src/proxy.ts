5d097ba9c853bc5573e8fa34b71df3d9348c8a85
trmtanjil <trmtanjil02@gmail.com>
2026-01-21 11:13:04 +0600

    deshboard allowd for authenticated person

--- nextJS/my-nextjs-cliend/src/proxy.ts
+++ nextJS/my-nextjs-cliend/src/proxy.ts
@@ -1,9 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
+import { userService } from "./services/user.service";
+import { Roles } from "./constentse/roles";
 
-export function proxy(request:NextRequest){
+export async function proxy(request:NextRequest){
 
-    console.log(request.url)
+    let isAuthenticated = false;
+    let isAdmin = false;
 
+
+    const {data} =await userService.getSession()
+
+    if(data){
+        isAuthenticated=true;
+        isAdmin=data.user.role=== Roles.admin
+    }
+
+    if(!isAuthenticated){
+        return NextResponse.redirect(new URL("/login",request.url))
+    }
+    
 return NextResponse.next()
 }
 
