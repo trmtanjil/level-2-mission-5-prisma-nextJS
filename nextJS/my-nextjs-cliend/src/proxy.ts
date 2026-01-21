369d120bf0a965903983797241f27ecef2e44ff2
trmtanjil <trmtanjil02@gmail.com>
2026-01-21 11:47:21 +0600

    Private routes using proxy.ts

--- nextJS/my-nextjs-cliend/src/proxy.ts
+++ nextJS/my-nextjs-cliend/src/proxy.ts
@@ -3,7 +3,8 @@ import { userService } from "./services/user.service";
 import { Roles } from "./constentse/roles";
 
 export async function proxy(request:NextRequest){
-
+    
+    const pathName = request.nextUrl.pathname
     let isAuthenticated = false;
     let isAdmin = false;
 
@@ -14,14 +15,29 @@ export async function proxy(request:NextRequest){
         isAuthenticated=true;
         isAdmin=data.user.role=== Roles.admin
     }
-
+console.log(isAdmin)
     if(!isAuthenticated){
         return NextResponse.redirect(new URL("/login",request.url))
     }
     
+    //chack admin 
+    if(isAdmin && pathName.startsWith("/deshboard")){
+        return NextResponse.redirect(new URL("/admin-deshboard",request.url))
+    }
+
+    //check user
+    if(!isAdmin && pathName.startsWith("/admin-deshboard"))
+{
+    return NextResponse.redirect(new URL("/deshboard",request.url))
+}
+
+    console.log("from proxy",pathName)
 return NextResponse.next()
 }
 
 export  const config ={
-    matcher:["/deshboard"]
+    matcher:["/deshboard",
+        "/deshboard/:path*",
+        "/admin-deshboard",
+        "/admin-deshboard/:path*"]
 } 
