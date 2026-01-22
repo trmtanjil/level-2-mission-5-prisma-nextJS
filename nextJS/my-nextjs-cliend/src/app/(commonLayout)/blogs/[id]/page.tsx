b58e3a365c6524d327ae42d535299ea019a22fff
trmtanjil <trmtanjil02@gmail.com>
2026-01-22 12:33:01 +0600

    dynamic route pag show id

--- nextJS/my-nextjs-cliend/src/app/(commonLayout)/blogs/[id]/page.tsx
+++ nextJS/my-nextjs-cliend/src/app/(commonLayout)/blogs/[id]/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
-export default function blogPage() {
+export default async function BlogPage({params}:
+  {params:Promise<{id:string}>}) {
+
+    const {id} = await params
+
   return (
-    <div>this is blogPage</div>
+    <div>this is blogPage{id}</div>
   )
 }
